8c95d8154f763082f3b2121a04ac9087d12686ca
Fernando Borretti <fernando@borretti.me>
2023-02-04 12:31:27 +1100

    parse_title

--- clippings.py
+++ clippings.py
@@ -2,7 +2,7 @@
 """
 Converts Kindle clippings ('My Clippings.txt') into JSON, CSV, or Markdown.
 """
-from re import Pattern, compile
+from re import Match, Pattern, compile, match
 
 # Clippings are separated by this string.
 CLIPPINGS_DELIMITER: str = "==========\n"
@@ -11,3 +11,20 @@ CLIPPINGS_DELIMITER: str = "==========\n"
 EPUB_TITLE_RE: Pattern[str] = compile(r"^(.+) \((.*)\)$")
 # Calibre PDFs have titles of the form `$title - $author`.
 PDF_TITLE_RE: Pattern[str] = compile(r"^(.+) - (.*)$")
+
+
+def parse_title(title_line: str) -> str:
+    # Try parsing an EPUB-style title.
+    matches: Match[str] = match(EPUB_TITLE_RE, title_line)
+    if matches:
+        return matches.group(1).strip()
+    # Try parsing a Calibre-style title.
+    matches = match(PDF_TITLE_RE, title_line)
+    if matches:
+        title: str = matches.group(1).replace("_ ", ": ").strip()
+        if title.endswith(", The"):
+            return "The " + title.replace(", The", "")
+        else:
+            return title
+    # On failure, return the title line unchanged.
+    return title_line.strip()
