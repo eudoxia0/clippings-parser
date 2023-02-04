5d855f719a938847577930fbd70d56e59c4c893e
Fernando Borretti <fernando@borretti.me>
2023-02-04 12:30:35 +1100

    Constants

--- clippings.py
+++ clippings.py
@@ -2,3 +2,12 @@
 """
 Converts Kindle clippings ('My Clippings.txt') into JSON, CSV, or Markdown.
 """
+from re import Pattern, compile
+
+# Clippings are separated by this string.
+CLIPPINGS_DELIMITER: str = "==========\n"
+
+# Epub books and such have titles of the form `$title ($author)`.
+EPUB_TITLE_RE: Pattern[str] = compile(r"^(.+) \((.*)\)$")
+# Calibre PDFs have titles of the form `$title - $author`.
+PDF_TITLE_RE: Pattern[str] = compile(r"^(.+) - (.*)$")
